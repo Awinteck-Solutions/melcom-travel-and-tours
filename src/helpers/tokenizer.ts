cee4ee20a19bb963e45b2c941ae7946870e96578
Abel Nutsugah <bigmanabel@outlook.com>
2025-09-19 14:43:57 -0700

    refactor: convert synchronous bcrypt methods to asynchronous

--- src/helpers/tokenizer.ts
+++ src/helpers/tokenizer.ts
@@ -6,13 +6,13 @@ dotenv.config();
 const { JWT_SECRET = "" } = process.env;
 export class encrypt {
   static async encryptpass(password: string) {
-    return bcrypt.hashSync(password, 12);
+    return await bcrypt.hash(password, 12);
   }
-  static comparepassword(hashPassword: string, password: string) {
-    return bcrypt.compareSync(password, hashPassword);
+  static async comparepassword(password: string, hashPassword: string) {
+    return await bcrypt.compare(password, hashPassword);
   }
 
-  static generateToken(payload: any) {
-    return jwt.sign(payload, JWT_SECRET, { expiresIn: "1d" });
+  static async generateToken(payload: any) {
+    return await jwt.sign(payload, JWT_SECRET, { expiresIn: "1d" });
   }
 }
