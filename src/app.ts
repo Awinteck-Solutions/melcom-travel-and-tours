3ddd3f612b1f76c4d94625d2d28c9a56901ddafc
Abel Nutsugah <bigmanabel@outlook.com>
2025-10-10 09:06:08 -0700

    feat: update port configuration in docker-compose.yml and app.ts for consistency

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ app.get("/", (req: Request, res: Response) => {
 connectToDatabase()
   .then(() => {
     app.listen(process.env.PORT, () =>
-      console.log("Server running on port 3000")
+      console.log("Server running on port " + process.env.PORT)
     );
   })
   .catch((error) => {
