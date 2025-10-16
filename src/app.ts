979c6346eb0f4e11e49abce49ef18aa674382e75
Samjay Awinteck <basampana@st.ug.edu.gh>
2025-10-16 14:40:22 +0000

    fix auth

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 
 app.get("/", (req: Request, res: Response) => {
   res.json({
-    message: "Welcome to Melcom Travel & Tours API",
+    message: "Welcome to Melcom Travel & Tours API v1.0.0",
     version: "1.0.0",
     endpoints: {
       flights: {
@@ -55,7 +55,7 @@ app.get("/", (req: Request, res: Response) => {
 connectToDatabase()
   .then(() => {
     app.listen(process.env.PORT, () =>
-      console.log("Server running on port " + process.env.PORT)
+      console.log("Server running on port " + process.env.PORT, 'v1.0.0')
     );
   })
   .catch((error) => {
