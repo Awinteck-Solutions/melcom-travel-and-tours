4b3bddd194c31edde46b3855ba0493d98004077e
Samjay Awinteck <basampana@st.ug.edu.gh>
2025-11-18 17:14:24 +0000

    test gol

--- src/Features/userCheckout/controllers/userCheckout.controller.ts
+++ src/Features/userCheckout/controllers/userCheckout.controller.ts
@@ -360,8 +360,7 @@ export class UserCheckoutController {
               },
             ],
           },
-          ParameterGroup: [
-            {
+          ParameterGroup: {
               Code: "passengerPerson",
               ParameterElement: [
                 {Name: "passenger_title", $t: traveler.NamePrefix || "MR"},
@@ -376,19 +375,8 @@ export class UserCheckoutController {
                   Format: "ascii_alphabet",
                 },
               ],
-            },
-            {
-              Code: "passengerServices",
-              ParameterElement: [
-                {Name: "passenger_frequent_flyer", $t: ""},
-                {
-                  Name: "passenger_frequent_flyer_number",
-                  $t: "",
-                  Format: "alphanumeric",
-                },
-              ],
-            },
-          ],
+            }
+          ,
         })
       );
 
